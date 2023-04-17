1b14471ec5ac0041b9945423fac89aaaf466c438
Jesse Zhou <22465982@student.uwa.edu.au>
2023-04-17 21:19:25 +0800

    Added a file containing a function for checking that the rotational order of vertices generates isn't violated, and also made the new main program in main.jl, including the rotational order checker

---
+++ rot_ord_check.jl
@@ -0,0 +1,18 @@
+function rot_ord_check(origin, points)
+	all_good = 1
+	flicked = 0
+	vec_angles = []
+	for i in 1:length(points)
+		vec_to_point = [points[i][1][1] - origin[1], points[i][1][2] - origin[2]]
+		angle_of_vec = atan(vec_to_point[2], vec_to_point[1])
+		push!(vec_angles, angle_of_vec)
+		if(i != 1 && vec_angles[i] < vec_angles[i-1])
+			if(flicked == 0)
+				flicked = 1
+			else
+				all_good = 0
+			end
+		end
+	end
+	return all_good
+end
