74c5683fb84b05995c6b0260bd9b61ffa6b753cb
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2023-06-12 19:24:29 +0800

    Just simulated circ_init for 200 time steps. The results are...strange. So the main code, whihc only changes the initialisation function, is from circ_init.jl. This will be called h4

--- rot_ord_check.jl
+++ rot_ord_check.jl
@@ -6,7 +6,7 @@ function rot_ord_check(origin::Tuple{Float64, Float64}, points::Vector{Tuple{Tup
 		vec_to_point = [points[i][1][1] - origin[1], points[i][1][2] - origin[2]]
 		angle_of_vec = atan(vec_to_point[2], vec_to_point[1])
 		push!(vec_angles, angle_of_vec)
-		if(i != 1 && vec_angles[i] < vec_angles[i-1] && norm((points[i][1][1] - points[i-1][1][1], points[i][1][2]-points[i-1][1][2])) > 10^(-9))
+		if(i != 1 && vec_angles[i] < vec_angles[i-1] && norm((points[i][1][1] - points[i-1][1][1], points[i][1][2]-points[i-1][1][2])) > 10^(-7))
 			if(flicked == 0)
 				flicked = 1
 			else
