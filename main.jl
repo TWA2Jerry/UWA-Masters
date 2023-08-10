f455ebdea4041fbd407537524184604ec412ddd5
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2023-08-10 08:40:44 +0800

    h58. Simulated creating a crystallised state, then booting up with a higher tdod. It seems that we can break out of crystallised state doing so

--- main.jl
+++ main.jl
@@ -37,7 +37,7 @@ const no_birds::Int32 = 100
 const rho::Float64 = 100.0
 initialised::Int32 = 0
 area_zero = zeros(Int32, 100)
-const rect_bound::Float64 = 10000.0
+const rect_bound::Float64 = 1000.0
 const spawn_dim_x::Float64 = 100.0 #This gives the x dimesnion size of the initial spawning area for the agents
 const spawn_dim_y::Float64 = 100.0 #This gives the y dimension size of the initial spawning area for the agents
 rect = Rectangle(Point2(0,0), Point2(Int64(rect_bound), Int64(rect_bound)))
@@ -94,8 +94,10 @@ function initialise(; target_area_arg = 1000*sqrt(12), simulation_number_arg = 1
 	pack_positions = Vector{Point2{Float64}}(undef, no_birds)
 	
 	#Initialise the positions based on the spawn-error free function of assign_positions
-	assign_positions(2.0, 2.0, no_birds, spawn_dim_x, spawn_dim_y, (rect_bound-spawn_dim_x)/2, (rect_bound-spawn_dim_x)/2, initial_positions)
-
+	#assign_positions(2.0, 2.0, no_birds, spawn_dim_x, spawn_dim_y, (rect_bound-spawn_dim_x)/2, (rect_bound-spawn_dim_x)/2, initial_positions)
+	for i in 1:no_birds
+		push!(initial_positions,last_pos_array[i])
+	end
 	for i in 1:no_birds
 		#rand_position = Tuple(100*rand(Float64, 2)) .+ (50.0, 50.0) 
 		rand_vel::Tuple{Float64, Float64} = 2 .* Tuple(rand(Float64, 2)) .- (1.0, 1.0)
@@ -394,7 +396,12 @@ function run_ABM(i, target_area) #Note that we're asking to input no simulations
 	write(rot_o_file, "\n")
 	write(rot_o_alt_file, "\n")
 	write(mean_speed_file, "\n")
-
+	#=last_pos_file = open("last_pos.txt", "w")
+	for i in 1:nagents(model)
+		pos = model[i].pos
+		write(last_pos_file, "$(pos[1]) $(pos[2])\n")
+	end
+	close(last_pos_file) =# 
 end #This should be the end of the function or running the ABM
 
 ###This line simulates the model
--- program.jl
+++ program.jl
@@ -1,5 +1,5 @@
 const no_simulations::Int64 = 1
-const no_steps::Int64 = 50
+const no_steps::Int64 = 5000
 
 ###Define IO. files
 compac_frac_file = open("compaction_frac.txt", "w")
@@ -8,6 +8,7 @@ rot_o_file = open("rot_order.txt", "w")
 rot_o_alt_file = open("rot_order_alt.txt", "w")
 mean_speed_file = open("mean_speed.txt", "w")
 rot_alt_target_ave_file = open("rot_order_alt_tave.txt", "w")
+last_pos_file = open("last_pos.txt", "r")
 
 ###Load the functions
 include("io_file.jl")
@@ -15,9 +16,16 @@ include("main.jl")
 
 ###Run ABM
 
+last_pos_array = []
+last_pos_lines = readlines(last_pos_file)
+for line in last_pos_lines
+	split_line = parse.(Float64, split(line, " "))
+	push!(last_pos_array, (split_line[1], split_line[2]))
+end
+
 for i in 1:no_simulations
 	run_ABM(i, sqrt(12)*1000.0)
 end
 
 do_io_stuff(compac_frac_file, mean_a_file, rot_o_file, rot_o_alt_file, mean_speed_file)
-do_more_io_stuff(adf, mdf)
+#do_more_io_stuff(adf, mdf)
