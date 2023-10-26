75e289187b12cda2851e02f25ebb779664eda76f
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2023-10-26 17:08:39 +0800

    h112. NOt really anything, just testing if we still get stationary agents with no turning, stdod model and angular conflict. Seems to be the case, so might turn turning back on, but in a determininstic manner

--- move_gradient_file.jl
+++ move_gradient_file.jl
@@ -47,11 +47,9 @@ function move_gradient(agent, model::UnremovableABM{ContinuousSpace{2, true, Flo
 		
 		no_angles_considered += 1
 		for j::Int64 in 1:m #For every position up to m
-			#=
 			if(angular_conflict == 1) 
 				break
 			end
-			=#
 
 			conflict::Int64 = 0
 			new_agent_pos::Tuple{Float64, Float64} = agent.pos .+ j .* direction_of_move .* agent_speed .* dt
