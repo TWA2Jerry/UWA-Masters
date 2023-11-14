c775b10138c94b87a064c4ed2dd93b97acc67c32
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2023-11-14 16:33:57 +0800

    i17. Just i16 but with no more text

--- io_file.jl
+++ io_file.jl
@@ -151,9 +151,12 @@ function draw_figures(model::UnremovableABM{ContinuousSpace{2, true, Float64, ty
         
 	
 	figure, ax, colourbarthing = Makie.scatter([Tuple(point) for point in new_pos], axis = (; title = "Model state at step $(model.n)", limits = (0, rect_bound, 0, rect_bound)), marker = '→', markersize = 20, rotations = rotations, color = colours, colormap = cgrad(:matter, 300, categorical = true), colorrange = (0, 300))
+	#=
 	for i in 1:nagents(model)
                 text!(new_pos[i], text = "$i", align = (:center, :top))
         end
+	=#	
+
         #print("The number of points in path points is $(length(path_points))\n")
 	draw_path(path_points)
 	title!("Model state at step $(model.n)")
