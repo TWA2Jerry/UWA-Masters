a8ced46ac9de36082effca0f3c84a51337826e30
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-02-14 21:40:16 +0800

    stdod13. Stuff is in stdod12 folder though. Simulated stdod model excluding neighbours behind an agent. tdod of 0.0. No significant effects observed.

--- program.jl
+++ program.jl
@@ -25,7 +25,7 @@ end
 adata = [happiness, :true_A, :perimeter_squared, :no_neighbours, :rot_o_alt, :rot_o_alt_corr, agent_regularity]
 mdata = [mean_radial_distance, rot_o_alt, random_happiness, mean_no_moves, polarisation, random_radius, mean_happiness, rot_o, mean_no_neighbours, no_collabs]
 
-target_dods = [1000.0*sqrt(12)]
+target_dods = [0.0]
 
 parameters = Dict(
         :seed => [i for i::Int64 in 1:no_simulations],
