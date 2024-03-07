dc3644ace9f94420150a9b72237bd8776c45e6d2
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-03-07 11:14:39 +0800

    stdod21. Simulated stdod model with tdod = 4000*sqrt(12) to see unhappiness distribution and occupation numbers

--- global_vars.jl
+++ global_vars.jl
@@ -1,7 +1,7 @@
 const no_birds::Int32 = 100
 initialised::Int32 = 0
 area_zero = zeros(Int32, 100)
-const rect_bound::Float64 = 1000.0
+const rect_bound::Float64 = 10000.0
 const spawn_dim_x::Float64 = 100.0 #This gives the x dimesnion size of the initial spawning area for the agents
 const spawn_dim_y::Float64 = 100.0 #This gives the y dimension size of the initial spawning area for the agents
 const rho::Float64 = 100.0
@@ -17,4 +17,4 @@ best_pos::Vector{Tuple{Float64, Float64}} = Vector{Tuple{Float64, Float64}}(unde
 eta::Float64 = 1.0
 const lower_area::Float64 = 0.0
 const upper_area::Float64 = 500.0
-bin_range::Tuple{Float64, Float64} = (0.195, 0.2)
+bin_range::Tuple{Float64, Float64} = (0.19, 0.2)
