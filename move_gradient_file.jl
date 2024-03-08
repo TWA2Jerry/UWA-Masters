a6baf9590de59a144e93192320cce16cc64c7625
TWA2Jerry <jesse@dep56725.uniwa.uwa.edu.au>
2024-03-08 16:47:37 +0800

    Misc. commit from the UWA laptop. Seems to work.

--- move_gradient_file.jl
+++ move_gradient_file.jl
@@ -363,6 +363,8 @@ function move_gradient_alt(agent, model::UnremovableABM{ContinuousSpace{2, true,
 			=#
 
 			#if (abs(new_area-target_area) < min_diff && conflict != 1)
+			lower_area::Float64 = model.lower_area
+			upper_area::Float64 = model.upper_area
 			if((new_area > lower_area && new_area < upper_area && j < min_distance) || (move_made == 0 && abs(new_area-target_area) < min_diff && conflict != 1))	
 				min_diff = abs(new_area-target_area)
 				min_area = new_area
