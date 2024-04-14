a75a8804e8230d4d8ace18f527237438ccf3f2dc
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-04-14 20:50:00 +0800

    Just amending some of the graphs from stdod41-43.

--- Plotting/plot_happiness_histogram.jl
+++ Plotting/plot_happiness_histogram.jl
@@ -1,1 +1,1 @@
-Makie.hist(adf[200000:500100, 3], bins = range(0.0, 1.0, 100), normalization = :probability, axis =(xticklabelsize = 30, yticklabelsize=30, xlabelsize = 40, ylabelsize=40, ylabel = "Frequency", xlabel = "Unhappiness"))
+Makie.hist(adf[200000:500100, 3], bins = range(0.0, 1.0, 100), normalization = :probability, axis =(xticklabelsize = 30, yticklabelsize=30, xlabelsize = 40, ylabelsize=40, ylabel = "Relative Frequency", xlabel = "Unhappiness"))
--- program.jl
+++ program.jl
@@ -6,7 +6,7 @@ include("prog.h")
 adata = [happiness, :true_A, :perimeter_squared, :no_neighbours, :rot_o_alt, :rot_o_alt_corr, agent_regularity, :distance]
 mdata = [mean_radial_distance, rot_o_alt, random_happiness, mean_no_moves, polarisation, random_radius, mean_happiness, rot_o, mean_no_neighbours, no_collabs, num_in_bin, mean_speed]
 
-target_dods = [4000.0*sqrt(12)]
+target_dods = [0.0*sqrt(12)]
 
 parameters = Dict(
         :simulation_number_arg => [i for i::Int64 in 1:no_simulations],
