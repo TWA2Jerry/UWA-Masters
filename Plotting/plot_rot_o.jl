24170f7ee7f5920b43780a529e657ee18bb2953e
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-04-14 21:42:59 +0800

    mv19. Simulated mv with lower and upper areas of (1.0*sqrt(12), 1000*sqrt(12)-1000.0). Definitely lower rot_o

--- Plotting/plot_rot_o.jl
+++ Plotting/plot_rot_o.jl
@@ -1,1 +1,1 @@
-Makie.lines(mdf[1:10000, 9], axis =(xticklabelsize = 30, yticklabelsize=30, xlabelsize = 40, ylabelsize=40, ylabel = "Relative Frequency", xlabel = "Optimal Area"))
+Makie.lines(mdf[1:10000, 9], axis =(xticklabelsize = 30, yticklabelsize=30, xlabelsize = 40, ylabelsize=40, ylabel = "Rotational order", xlabel = "Step"))
