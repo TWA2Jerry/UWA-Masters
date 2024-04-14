2ebdcf79a050756c4a98e31d1ac98a0809d46c32
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-04-14 13:01:21 +0800

    mv18. Simulated mv with normal parameters, but upper_area_arg is decreased by 100 for small perturbation away from peak of rot_o. Histogram of desired areas plotted

---
+++ Plotting/plot_rot_o.jl
@@ -0,0 +1,1 @@
+Makie.lines(mdf[1:10000, 9], axis =(xticklabelsize = 30, yticklabelsize=30, xlabelsize = 40, ylabelsize=40, ylabel = "Relative Frequency", xlabel = "Optimal Area"))
--- program.jl
+++ program.jl
@@ -28,7 +28,7 @@ parameters = Dict(
 	:area_args => lower_upper_areas,	
 )
 
-model = initialise(target_area_arg = 100.0, simulation_number_arg = 1, no_bird = no_birds)
+model = initialise(target_area_arg = 100.0, simulation_number_arg = 1, no_bird = no_birds, area_args = (sqrt(12), 1000*sqrt(12)-100.0))
 adf, mdf = @time run!(model, agent_step!, model_step!, no_steps; adata, mdata)
 
 ###New thingo for running, just because there's never reason you wouldn't use this general method of running possibly multiple params
