3b81bca1aa51d892a4a82e6a79952dd5d8f0b863
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-04-15 22:14:28 +0800

    mv24 and mv25. Both do longer runs of mv, normal parameter set. Just testing to see if high rot o scheme is stable

--- Plotting/plot_rot_o.jl
+++ Plotting/plot_rot_o.jl
@@ -1,1 +1,1 @@
-Makie.lines(mdf[1:10000, 9], axis =(xticklabelsize = 30, yticklabelsize=30, xlabelsize = 40, ylabelsize=40, ylabel = "Rotational order", xlabel = "Step"))
+Makie.lines(mdf[:, 9], axis =(xticklabelsize = 30, yticklabelsize=30, xlabelsize = 40, ylabelsize=40, ylabel = "Rotational order", xlabel = "Step"))
