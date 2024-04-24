11028744ce2e6dc35b1a202896e84a15dc2c1170
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-04-24 11:35:41 +0800

    mv37. mv with volume exclusion turned back on, area_args = (1*stdod, 1500*stdod). Some rotational order seen?

--- program.jl
+++ program.jl
@@ -28,7 +28,7 @@ parameters = Dict(
 	:area_args => lower_upper_areas,	
 )
 
-model = initialise(target_area_arg = 100.0, simulation_number_arg = 1, no_bird = no_birds, area_args = (1*sqrt(12), 2000.0*sqrt(12)))
+model = initialise(target_area_arg = 100.0, simulation_number_arg = 1, no_bird = no_birds, area_args = (1*sqrt(12), 1500.0*sqrt(12)))
 adf, mdf = @time run!(model, agent_step!, model_step!, no_steps; adata, mdata)
 
 ###New thingo for running, just because there's never reason you wouldn't use this general method of running possibly multiple params
