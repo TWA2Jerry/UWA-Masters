ef218a497888c4a75a460581cc791917eb96fd1d
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-05-12 14:54:33 +0800

    mv50. Tried to eliminate any possible further bias by removing clause for allowing move if no move has been made. Strong rot_o_alt is still observed.

--- move_gradient_file.jl
+++ move_gradient_file.jl
@@ -368,7 +368,7 @@ function move_gradient_alt(agent, model::UnremovableABM{ContinuousSpace{2, true,
 			#if (abs(new_area-target_area) < min_diff && conflict != 1)
 			lower_area::Float64 = model.lower_area
 			upper_area::Float64 = model.upper_area
-			if((new_area > lower_area && new_area < upper_area && j <  min_distance) || (move_made == 0 && abs(new_area-target_area) < min_diff && conflict != 1))	
+			if((new_area > lower_area && new_area < upper_area && j <  min_distance))	
 			#if((new_area > lower_area && new_area < upper_area && j < min_distance) || (move_made == 0 && abs(new_area-target_area) < min_diff))
 				
 				min_diff = abs(new_area-target_area)
