314d920608495f216842f51e74ee9748fe6b0d83
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-06-10 21:31:41 +0800

    mv67. mv with l=1*stdod, r=2000*stdod (from now on called cluster configuration), 25 agents. Nothing special observed.

--- Plotting/plot_rot_o.jl
+++ Plotting/plot_rot_o.jl
@@ -1,2 +1,2 @@
 using LaTeXStrings
-fig, ax = Makie.lines(mdf[:, 5], axis =(xticklabelsize = 30, yticklabelsize=30, xlabelsize = 40, ylabelsize=40, ylabel = L"\textrm{Rotational order } (\Phi_R)", xlabel = "Step", xgridvisible = false, ygridvisible = false))
+fig, ax = Makie.lines(mdf[:, 5], axis =(xticklabelsize = 30, yticklabelsize=30, xlabelsize = 40, ylabelsize=40, ylabel = L"\textrm{Rotational order } (\Phi_{R*})", xlabel = "Step", xgridvisible = false, ygridvisible = false))
