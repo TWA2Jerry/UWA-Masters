c49123b83776883b4df579d8a25f9d0774226492
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-06-18 13:34:05 +0800

    Just redid mv82 with 22 agents, just to check numbers. Seems right. So now, second column of the .txt file is the ave_group_rot_o, 3rd col is the stdev of ave_group rot_o, and 4th column is the original mean of the rot_o

---
+++ Plotting/plot_size_rot_o.jl
@@ -0,0 +1,16 @@
+using Plots
+using CairoMakie
+
+rot_o_l_bias_file = open("Records/mv82/rot_o_v_no_bird.txt", "r")
+l_bias_rot_o_lines = readlines(rot_o_l_bias_file)
+
+bias_rot_o_array = Vector{Tuple{Float64, Float64}}(undef, 0)
+
+for line in l_bias_rot_o_lines
+        split_line = parse.(Float64, split(line, " "))
+        push!(bias_rot_o_array, (split_line[1], split_line[2]))
+end
+
+#Makie.plot(bias_rot_o_array)
+#fig = Plots.plot(bias_rot_o_array)
+fig = Makie.scatter(bias_rot_o_array)
