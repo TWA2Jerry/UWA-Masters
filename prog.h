a8f0b1367620c358cffd07cc752bf08037fe6c30
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-07-17 15:54:44 +0800

    mv97. Simulated mv with liquid-liquid config, i.e, both lower and upper bounds in same phase. Video for talk. Might've also deleted mv55

--- prog.h
+++ prog.h
@@ -1,5 +1,5 @@
-const no_simulations::Int64 = 10
-const no_steps::Int64 = 20000
+const no_simulations::Int64 = 5
+const no_steps::Int64 = 5000
 
 ###Define IO. files
 compac_frac_file = open("compaction_frac.txt", "w")
