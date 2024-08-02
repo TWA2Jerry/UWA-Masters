0831aaa6234f751738be3b2aa62a9311294c4fee
Jesse Mingyang Zhou <UNIWA\22465982@dep63129.uniwa.uwa.edu.au>
2024-08-02 14:51:59 +0800

    gt37. Just showing that we can move in directions other than left

--- program.jl
+++ program.jl
@@ -1,20 +1,7 @@
 const no_simulations::Int64 = 1
-const no_steps::Int64 = 10000
+const no_steps::Int64 = 1000
+include("prog.h")
 
-###Define IO. files
-#=compac_frac_file = open("compaction_frac.txt", "w")
-mean_a_file = open("mean_area.txt", "w")
-rot_o_file = open("rot_order.txt", "w")
-rot_o_alt_file = open("rot_order_alt.txt", "w")
-mean_speed_file = open("mean_speed.txt", "w")
-rot_alt_target_ave_file = open("rot_order_alt_tave.txt", "w")
-pos_vels_file = open("pos_vels.txt", "w")
-
-###Load the functions
-include("order_parameters.jl")
-include("io_file.jl")
-include("main.jl")
-=#
 ###Run ABM
 #=
 for i in 1:no_simulations
