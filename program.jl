c33bad0683bc968bb5d33b66612044a722bb0aee
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-08-29 23:17:14 +0800

    stdod56. Configuration for forward and circle bounded cell illustration for thesis.

--- program.jl
+++ program.jl
@@ -16,7 +16,7 @@ parameters = Dict(
 ) 
 
 model = initialise(target_area_arg = 1000.0*sqrt(12), simulation_number_arg = 1, no_bird = no_birds)
-adf, mdf = @time run!(model, agent_step!, model_step!, no_steps; adata, mdata)
+#adf, mdf = @time run!(model, agent_step!, model_step!, no_steps; adata, mdata)
 
 ###New thingo for running, just because there's never reason you wouldn't use this general method of running possibly multiple params
 #adf, mdf  = paramscan(parameters, initialise; adata, mdata, agent_step!, model_step!, n = no_steps)
