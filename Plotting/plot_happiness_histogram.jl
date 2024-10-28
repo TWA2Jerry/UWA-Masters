ebf81ab8bf4841a81620bee92cd340584b1db856
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-10-28 15:21:39 +0800

    Added picture of happiness histogram for stdod60, with updated function in plot_happiness_histogram.jl

--- Plotting/plot_happiness_histogram.jl
+++ Plotting/plot_happiness_histogram.jl
@@ -1,1 +1,6 @@
 Makie.hist(adf[200000:500100, 3], bins = range(0.0, 1.0, 100), normalization = :probability, axis =(xticklabelsize = 30, yticklabelsize=30, xlabelsize = 40, ylabelsize=40, ylabel = "Relative Frequency", xlabel = "Unhappiness"))
+
+function plot_happiness_hist(filtered_data)
+	fig, ax = Makie.hist(filtered_data[:, 3], bins = range(0.0, 1.0, 100), normalization = :probability, axis =(xticklabelsize = 30, yticklabelsize=30, xlabelsize = 40, ylabelsize=40, ylabel = "Relative Frequency", xlabel = "Unhappiness"))
+	return fig, ax
+end
