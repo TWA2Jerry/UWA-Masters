37a25f2f5ed7108d45544af82b9be5bc327531eb
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-11-06 18:24:59 +0800

    Fixed Plotting/line_plot_template.jl to allow adjustments to margins

--- Plotting/line_plot_template.jl
+++ Plotting/line_plot_template.jl
@@ -2,32 +2,32 @@ using CairoMakie
 using Plots
 
 function give_line_plot(data_points; label_arg = "", xlabel_arg = "", ylabel_arg = "", linewidth_arg = 10, labelvisible_arg = true, limits_arg = (nothing, nothing), figure_padding_arg = (:auto, :auto, :auto, :auto))
-	fig, ax, thing = Makie.lines(data_points,
-		linewidth = linewidth_arg,
-		axis = (;
-			limits = limits_arg,
-			#width = 900,
-			xlabelsize = 50,
-			ylabelsize = 50,
-			xlabel = xlabel_arg,
-			ylabel = ylabel_arg,
-			xticklabelsize = 30,
-			yticklabelsize = 30,
-			xgridvisible = false,
-			ygridvisible = false,
-			#xlabelpadding = 2.0
-		),
+	fig = Figure(figure_padding = figure_padding_arg)
+	ax = Axis(fig[1,1],
+		limits = limits_arg,
+            #width = 900,
+            xlabelsize = 50,
+            ylabelsize = 50,
+            xlabel = xlabel_arg,
+            ylabel = ylabel_arg,
+            xticklabelsize = 30,
+            yticklabelsize = 30,
+            xgridvisible = false,
+            ygridvisible = false,
+            #xlabelpadding = 2.0
+	)
 
-		label = label_arg,
-		#figure_padding = (:auto, :auto, :auto, :auto)
+	Makie.lines!(ax, data_points,
+		linewidth = linewidth_arg
 	)
 
 	if(labelvisible_arg == true)
-	axislegend(ax,
+		axislegend(ax,
 		labelsize = 30
 	)
+
 	end
-	return fig, ax, thing
+	return fig, ax
 end
 
 function give_line_plot!(data_points; label_arg = "", xlabel_arg = "", ylabel_arg = "")
