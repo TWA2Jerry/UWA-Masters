5e8911e7b8e9addd2aa97baae6df0aea758e25cb
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-11-14 14:26:22 +0800

    mv118 with 100 agents

--- Plotting/line_plot_template.jl
+++ Plotting/line_plot_template.jl
@@ -1,27 +1,33 @@
 using CairoMakie
 using Plots
 
-function give_line_plot(data_points; label_arg = "", xlabel_arg = "", ylabel_arg = "")
-	fig, ax, thing = Makie.lines(data_points,
-		linewidth = 10,
-		axis = (;
-			xlabelsize = 50,
-			ylabelsize = 50,
-			xlabel = xlabel_arg,
-			ylabel = ylabel_arg,
-			xticklabelsize = 30,
-			yticklabelsize = 30,
-			xgridvisible = false,
-			ygridvisible = false
-		),
-	
-		label = label_arg,
+function give_line_plot(data_points; label_arg = "", xlabel_arg = "", ylabel_arg = "", linewidth_arg = 10, labelvisible_arg = true, limits_arg = (nothing, nothing), figure_padding_arg = (:auto, :auto, :auto, :auto))
+	fig = Figure(figure_padding = figure_padding_arg)
+	ax = Axis(fig[1,1],
+		limits = limits_arg,
+            #width = 900,
+            xlabelsize = 50,
+            ylabelsize = 50,
+            xlabel = xlabel_arg,
+            ylabel = ylabel_arg,
+            xticklabelsize = 30,
+            yticklabelsize = 30,
+            xgridvisible = false,
+            ygridvisible = false,
+            #xlabelpadding = 2.0
 	)
 
-	axislegend(ax,
+	Makie.lines!(ax, data_points,
+		linewidth = linewidth_arg
+	)
+
+	if(labelvisible_arg == true)
+		axislegend(ax,
 		labelsize = 30
 	)
-	return fig, ax, thing
+
+	end
+	return fig, ax
 end
 
 function give_line_plot!(data_points; label_arg = "", xlabel_arg = "", ylabel_arg = "")
