eb8a79b94a54958997c57a075ca6e3ed85326d46
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-11-26 21:41:47 +0800

    Just adding and updating plotting file for plot of mv120 with arrow path as the marker.

--- Records/mv120/plot_model_rot_o.jl
+++ Records/mv120/plot_model_rot_o.jl
@@ -1,6 +1,6 @@
-include("../../load_randstep.jl")
+#include("../../load_randstep.jl")
+include("prog.h")
+#model = initialise(pos_vels_file, 5900)
+model =  AgentsIO.load_checkpoint("./Records/mv120/mv120_s5900")
 
-model = initialise(pos_vels_file, 5900)
-
-#fig, ax = return_thesis_figures(model, colourmap_arg = :cool, colourbarlabel_arg = L"\Phi_{R*}")
-fig, ax = return_thesis_figures(model, colourmap_arg = :cool, fig_box = ((2000, 3000), (7000, 8000)), colourbarlabel_arg = L"\Phi_{R*}")
+fig, ax = return_thesis_figures(model, colourmap_arg = :cool, fig_box = ((4000, 3000), (8000, 7000)), colourbarlabel_arg = L"\Phi_{R*}", marker = arrow_marker, marker_size = 5)
