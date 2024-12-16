e9b8b52f1dad5b026f37dcf6fd4257c524ad5fc9
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2024-12-16 12:31:22 +0800

    stdod70. Ran stdod model with 90 degree FOV, tdod=100*stdod.

--- program.jl
+++ program.jl
@@ -4,11 +4,11 @@ const no_steps::Int64 = 5000
 
 include("prog.h")
 
-adata = [:best_q]
+adata = [(normalised_true_area, StatsBase.mean)]
 mdata = [mean_speed, rot_o_alt, rot_o, polarisation]
 
-target_dods = [22000.0]
-qp_args = [4]
+target_dods = [100*sqrt(12)]
+qp_args = [1]
 
 parameters = Dict(
         :simulation_number_arg => [i for i::Int64 in 1:no_simulations],
@@ -17,11 +17,11 @@ parameters = Dict(
         #:left_bias_arg => left_biases
 ) 
 
-model = initialise(target_area_arg = 10.0*sqrt(12), simulation_number_arg = 1, no_bird = no_birds)
-adf, mdf = @time run!(model, agent_step!, model_step!, no_steps; adata, mdata)
+#model = initialise(target_area_arg = 10.0*sqrt(12), simulation_number_arg = 1, no_bird = no_birds)
+#adf, mdf = @time run!(model, agent_step!, model_step!, no_steps; adata, mdata)
 
 ###New thingo for running, just because there's never reason you wouldn't use this general method of running possibly multiple params
-#adf, mdf  = paramscan(parameters, initialise; adata, mdata, agent_step!, model_step!, n = no_steps)
+adf, mdf  = paramscan(parameters, initialise; adata, mdata, agent_step!, model_step!, n = no_steps)
 
 #do_io_stuff(compac_frac_file, mean_a_file, rot_o_file, rot_o_alt_file, mean_speed_file)
 #do_more_io_stuff(adf, mdf)
