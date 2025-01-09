8e987867d1a835bb122235359085bc70bee5f8df
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2025-01-09 14:14:29 +0800

    stdod70 with tdod= 1*stdod. Finally, reduction in mean speed over time.

--- program.jl
+++ program.jl
@@ -1,4 +1,4 @@
-const no_simulations::Int64 = 20
+const no_simulations::Int64 = 1
 const no_steps::Int64 = 5000
 
 
@@ -7,7 +7,7 @@ include("prog.h")
 adata = [(normalised_true_area, StatsBase.mean)]
 mdata = [mean_speed, rot_o_alt, rot_o, polarisation]
 
-target_dods = [22000.0]
+target_dods = [1.0*sqrt(12)]
 q_args = [8]
 qp_args = [1]
 m_args = [100]
